d3131087d1a7147b95d9aac8566cc2a2cac53bbf
psurmicki <p.surmicki1@gmail.com>
2026-07-07 23:23:48 +0200

    fix test and lint

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -1,2 +1,6 @@
 from flask import Flask
+from hello_world.views import register_routes
+
 app = Flask(__name__)
+
+register_routes(app)
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,22 +1,22 @@
-from hello_world import app
-from hello_world.formater import get_formatted
-from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
+
+from hello_world.formater import PLAIN, SUPPORTED, get_formatted
 
 
 moje_imie = "Przemek"
 msg = "Hello World!"
 
 
-@app.route('/')
-def index():
-    output = request.args.get('output')
-    if not output:
-        output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
+def register_routes(app):
+    @app.route('/')
+    def index():
+        output = request.args.get('output')
+        if not output:
+            output = PLAIN
 
+        return get_formatted(msg, moje_imie, output.lower())
 
-@app.route('/outputs')
-def supported_output():
-    return ", ".join(SUPPORTED)
+    @app.route('/output')
+    @app.route('/outputs')
+    def supported_output():
+        return ", ".join(SUPPORTED)
